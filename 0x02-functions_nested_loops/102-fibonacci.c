a78cfa89efc6dc057d56317fa46cf2573b1c9996
SerioussBlack <bernardnewman70@gmail.com>
2023-02-22 01:34:19 -0800

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -13,7 +13,7 @@ int main(void)
 	while (i < 50)
 	{
 	if (i == 0)
-	print("%ld", j);
+	printf("%ld", j);
 	else if (i == 1)
 	printf(", %ld", k);
 	else
@@ -24,6 +24,6 @@ int main(void)
 		}
 		++i;
 	}
-	print("\n");
+	printf("\n");
 	return (0);
 }
