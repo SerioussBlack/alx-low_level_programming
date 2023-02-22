aec672a1877c1f7ce4220122a7540f7924517585
SerioussBlack <bernardnewman70@gmail.com>
2023-02-21 18:05:08 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,7 +16,7 @@ void print_to_98(int n)
 	{
 		for (i = n ; i <= 98 ; i++)
 		{
-			printf("%d, ", i);
+			printf("%d ", i);
 		}
 			printf("\n");
 	}
@@ -24,7 +24,7 @@ void print_to_98(int n)
 	{
 		for (i = n ; i <= 98 ; i++)
 		{
-			printf("%d, ", i);
+			printf("%d ", i);
 		}
 			printf("\n");
 
@@ -33,11 +33,11 @@ void print_to_98(int n)
 	{
 		for (i = n ; i >= 98 ; i--)
 		{
-			printf("%d, ", i);
+			printf("%d ", i);
 		}
 			printf("\n");
 	}
 	else if (n == 98)
-		printf("%d, ", n);
+		printf("%d ", n);
 
 }
