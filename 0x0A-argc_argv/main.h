72df789eceb6166b73f14cf7eb6580c798dc6bcf
SerioussBlack <bernardnewman70@gmail.com>
2023-03-10 18:08:10 -0800

    task 0

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+int main(int __attribute__((__unused__)) argc, char *argv[]);
+int _putchar(char c);
+int main(int argc, char __attribute__((__unused__)) *argv[]);
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+
+#endif
